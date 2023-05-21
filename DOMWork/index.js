0e7db78d03c620f5d6cfb2fbfbe293253a1c7f8c
SethTML <lamarsthought@gmail.com>
2023-05-21 01:22:35 -0400

    DOMWork

--- DOMWork/index.js
+++ DOMWork/index.js
@@ -1,8 +1,9 @@
-const GroupText = document.querySelectorAll("GroupText");
+const GroupText = document.querySelectorAll(".GroupText");
 
 for(let i = 0 ; i < GroupText.length;i++){
-    console.log(i);
+    let texta = GroupText[i];
+    let color3 = [0,0,0 + (i * 110)]
+    let color = "rgb(" + color3[0]+","+color3[1]+","+color3[2]+")";
+    texta.setAttribute("style","color: " + color)
+    console.log(color);
 }
-
-console.log("alive");
-console.log(GroupText);
