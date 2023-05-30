9cf9ae87827b0bae5c8388f5d923264ac5055705
SethTML <lamarsthought@gmail.com>
2023-05-30 19:16:20 -0400

    GroupText

--- DOMWork/index.js
+++ DOMWork/index.js
@@ -6,4 +6,5 @@ for(let i = 0 ; i < GroupText.length;i++){
     let color = "rgb(" + color3[0]+","+color3[1]+","+color3[2]+")";
     texta.setAttribute("style","color: " + color)
     console.log(color);
+    texta.textContent = ("Round " + i)
 }
